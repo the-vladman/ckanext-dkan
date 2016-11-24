6c37c436fab1c9f7dd2f2e70e027f24a9c8d39df
Frank <akura11.tt@gmail.com>
2016-11-24 15:53:39 -0600

    Ajuste de fechas y campos a formatos de CKAN

--- ckanext/dkan/harvesters/dkanharvester.py
+++ ckanext/dkan/harvesters/dkanharvester.py
@@ -330,7 +330,7 @@ class DKANHarvester(CKANHarvester):
         try:
             date_correct_format = date.replace('Date changed\t', '')[4:] if last_modified else date[4:]
             date_object = datetime.datetime.strptime(date_correct_format, '%d/%m/%y - %I:%M')
-        except Exception e:
+        except Exception, e:
             log.debug(e)
 
         return date_object.strftime("%Y-%m-%dT%H:%M:%S.%f")
